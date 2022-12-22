8a58d2541a8f3afe7e6426bb7f23b1d7fc964577
mrakhman <robinbad1312@gmail.com>
2022-12-22 19:59:05 +0100

    3 bottom buttons + draft for API fetching (commented)

--- app/src/main/java/com/example/swiftycompanion/MainActivity.kt
+++ app/src/main/java/com/example/swiftycompanion/MainActivity.kt
@@ -2,31 +2,63 @@ package com.example.swiftycompanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.swiftycompanion.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var todoAdapter: TodoAdapter
+
+    private lateinit var navController: NavController
+    // TODO: Commented lines about binding are for API fetching
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root) // TODO: can I have the 2 of them here?
         setContentView(R.layout.activity_main)
 
-        todoAdapter = TodoAdapter(mutableListOf())
+//        fetchCurrencyData().start()
 
-        rvTodoItems.adapter = todoAdapter
-        rvTodoItems.layoutManager = LinearLayoutManager(this)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.searchFragment,
+                R.id.profilesFragment,
+                R.id.myProfileFragment
+            )
+        )
 
-        btnAddTodo.setOnClickListener {
-            val todoTitle = etTodoTitle.text.toString()
-            if(todoTitle.isNotEmpty()) {
-                val todo = Todo(todoTitle)
-                todoAdapter.addTodo(todo)
-                etTodoTitle.text.clear()
-            }
-        }
-        btnDeleteDoneTodos.setOnClickListener {
-            todoAdapter.deleteDoneTodos()
-        }
+        // Navigation menu
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+//    private fun fetchCurrencyData(): Thread {
+//        return Thread {
+//            val url = URL("https://open.er-api.com/v6/latest/aud")
+//            val connection = url.openConnection() as HttpURLConnection
+//
+//            if (connection.responseCode == 200) {
+//                val inputSystem = connection.inputStream
+//                println(inputSystem.toString())
+//            } else {
+//                binding.baseCurrency.text = "Failed Connection"
+//            }
+//        }
+//    }
+
+    // Navigation menu
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/swiftycompanion/adapters/SwiftyCompanionAdapter.kt
@@ -0,0 +1,23 @@
+//package com.example.swiftycompanion.adapters
+//
+//import android.view.ViewGroup
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class SwiftyCompanionAdapter : RecyclerView.Adapter<SwiftyCompanionAdapter.MyViewHolder> {
+//    class MyViewHolder : RecyclerView.ViewHolder() {
+//
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemCount(): Int {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
